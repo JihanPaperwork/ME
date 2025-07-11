713be1a1406861d4d3a238d8b86f4d4c3e503920
JihanPaperwork <jihanjihan05hum@gmail.com>
2025-07-11 17:17:22 +0700

    Refactor server.js for Vercel deployment and update API_BASE_URL to relative path

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 // src/services/api.js
-const API_BASE_URL = 'http://localhost:3000/api';
+const API_BASE_URL = '/api'; // Menggunakan path relatif
 import { getAuthToken, clearAuth } from '../utils/auth.js';
 
 const fetchWrapper = async (endpoint, options = {}) => {
